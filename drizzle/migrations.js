800f1a3b0b2d08ead6c9efaa06d18d8924470154
Guillaume Pouyat <guillaume.pouyat@protonmail.com>
2025-12-15 22:19:03 +0100

    🗃️ Seed quest "Couper du bois" with related exercises

--- drizzle/migrations.js
+++ drizzle/migrations.js
@@ -4,6 +4,7 @@ import m0000 from "./0000_flaky_riptide.sql";
 import m0001 from "./0001_narrow_giant_girl.sql";
 import m0002 from "./0002_seed_exercises.sql";
 import m0003 from "./0003_quick_thunderbird.sql";
+import m0004 from "./0004_seed_quest_couper_du_bois.sql";
 import journal from "./meta/_journal.json";
 
 export default {
@@ -13,5 +14,6 @@ export default {
     m0001,
     m0002,
     m0003,
+    m0004,
   },
 };
